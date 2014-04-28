9169e81c352ce920323bca18b82b94a06e53d669
Karun Arora <karunarora007@rocketmail.com>
2014-04-28 07:33:06 +0530

    finish

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## There are two functions used for the program i.e makecacheMatrix and cachesolve
+## Function 1 i.e makecacheMatrix will take as argument a matrix nd store the
+## inverse of it in an object using scoping rules i.e setting and getting 
+## environment variables
 
-## Write a short comment describing this function
+## second function will use first function..it returns the matrix inverse by first
+## checking it in cache if present immediately returns result nd save time.
+## if not it calculates the result nd then return the inverse
 
-makeCacheMatrix <- function(x = matrix()) {
+## makeCacheMatrix function is taking as argument a matrix nd then using functions 
+## inside scope of makeCacheMAtrix getting nd setting the values for Matrix
 
+makeCacheMatrix <- function(x = matrix()) 
+{
+        CacheMatrix <- NULL 
+        set<- function(y){
+                x <<- y
+                CacheMatrix <<- NULL
+        }
+        
+        get <- function() x
+        
+        setMatrix <- function(parmMatrix) CacheMatrix <<-parmMatrix
+        getMatrix <- function() CacheMatrix
+        list(set=set,get=get,setMatrix=setMatrix,getMatrix=getMatrix)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve is function which takes as argument matrix it checks whether
+## the inverse of matrix is alreadyin cache it returns it in very less time 
+## if not present then it sets the matrix compute inverse using solve function
+## nd then returns the value of inverse
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x) {
+        
+        CacheMatrix <- x$getMatrix()
+        if(!is.null(CacheMatrix)){
+                message("getting cached data")
+                return (CacheMatrix)
+                
+        }
+        data <- x$get()
+        CacheMatrix <- solve(data)
+        
+        x$setMatrix(CacheMatrix)
+        CacheMatrix        
         ## Return a matrix that is the inverse of 'x'
 }
